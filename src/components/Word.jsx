de71bfe1057951b6b7287b76ad687b492f1febd2
jgrissom <jgrissom91@gmail.com>
2021-08-23 18:54:38 -0500

    [1.03] Embed jsx expression using state data

--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 
 class Word extends Component {
-  state = {  }
+  state = {
+    word: 'banana',
+    color: 'yellow',
+  }
   render() { 
-    return ( <div>Hello World!</div> );
+    return (
+      <div>
+        { this.state.word }
+      </div>
+    );
   }
 }
 
