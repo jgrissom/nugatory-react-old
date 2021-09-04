4d700d05f6b6fd7c8604a14abbfa4a5d7575ddf8
jgrissom <jgrissom91@gmail.com>
2021-09-03 20:57:01 -0500

    [2.10] Controlled component - Word
    - Remove local state

--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 
 class Word extends Component {
-  state = {
-    word: this.props.word.word,
-    color: this.props.word.color,
-  }
   // helper method
   renderColor(color) {
     return (color === undefined || color === null ? 'black' : color);
@@ -12,8 +8,8 @@ class Word extends Component {
   render() { 
     console.log(this.props);
     return (
-      <div onClick={ () => this.props.onDelete(this.props.word.id) } className='Word' style={{ color:this.renderColor(this.state.color) }}>
-        { this.state.word }
+      <div onClick={ () => this.props.onDelete(this.props.word.id) } className='Word' style={{ color:this.renderColor(this.props.word.color) }}>
+        { this.props.word.word }
       </div>
     );
   }
