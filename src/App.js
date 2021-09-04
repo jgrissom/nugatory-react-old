702ac68a294d31e9ee2f7b04c5794316319b496e
jgrissom <jgrissom91@gmail.com>
2021-09-03 22:20:51 -0500

    [5.01] Convert App to functional component
    - Replace Component from import statement with useState, useEffect
    - Change class declaration to function
    - Store words, setWords in state
    - Add const in front of functions
    - Remove 'this' keyword
    - Replace setState with setWords
    - Replace componentDidMount with useEffect
    - Remove render()

--- src/App.js
+++ src/App.js
@@ -1,51 +1,50 @@
 // Repository:  nugatory-react
 // Author:      Jeff Grissom
 // Version:     5.xx
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Word from './components/Word';
 import Counter from './components/Counter';
 import NewWord from './components/NewWord';
 import './App.css';
 
-class App extends Component {
-  state = {
-    words: [
-      { id: 1, word: 'banana', color: 'yellow' },
-      { id: 2, word: 'apple', color: 'red' },
-      { id: 3, word: 'lime', color: 'green' },
-    ]
+const App = () => {
+  const [words, setWords] = useState([]);
+
+  const handleDelete = (wordId) => {
+    const mutableWords = words.filter(w => w.id !== wordId);
+    setWords(mutableWords);
   }
-  handleDelete = (wordId) => {
-    const words = this.state.words.filter(w => w.id !== wordId);
-    this.setState({ words:words });
-  }
-  handleAdd = (word, color) => {
-    const { words } = this.state;
+  const handleAdd = (word, color) => {
     const id = words.length === 0 ? 1 : Math.max(...words.map(word => word.id)) + 1;
     const mutableWords = words.concat({ id: id, word: word, color: color });
-    this.setState({ words:mutableWords });
+    setWords(mutableWords);
   }
-  componentDidMount() {
-    console.log("App mounted");
-  }
-  render() { 
-    const { words } = this.state;
-    return ( 
-      <div className="App">
-        <header className="App-header">
-          nugatory
-        </header>
-        { words.map(word => 
-          <Word 
-            key={ word.id } 
-            word={ word } 
-            onDelete={ this.handleDelete } />
-        )}
-        <Counter totalWords={ words.length } />
-        <NewWord onAdd={ this.handleAdd } />
-      </div>
-     );
-  }
+  // this is the functional equivalent to componentDidMount
+  useEffect(() => {
+    // initial data loaded here
+    let mutableWords = [
+      { id: 1, word: "banana", color: "yellow" },
+      { id: 2, word: "apple", color: "red" },
+      { id: 3, word: "lime", color: "green" },
+      { id: 4, word: "orange", color: "orange" }
+    ]
+    setWords(mutableWords);
+  }, []);
+  return ( 
+    <div className="App">
+      <header className="App-header">
+        nugatory
+      </header>
+      { words.map(word => 
+        <Word 
+          key={ word.id } 
+          word={ word } 
+          onDelete={ handleDelete } />
+      )}
+      <Counter totalWords={ words.length } />
+      <NewWord onAdd={ handleAdd } />
+    </div>
+    );
 }
  
 export default App;
