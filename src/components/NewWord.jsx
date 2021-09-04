d7adaa1405787d8dd1930a2ac80b21ea3b3e52d7
jgrissom <jgrissom91@gmail.com>
2021-09-03 21:44:06 -0500

    [4.03] Use conditional rendering to show/hide form

--- src/components/NewWord.jsx
+++ src/components/NewWord.jsx
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
 
 class NewWord extends Component {
+  state = {
+    showForm: false,
+  }
   render() { 
     return (
       <div className='New-word'>
-        <form>
-          <input 
-            type="text"
-            id="word"
-            placeholder="Word"
-            autoComplete="off" />
-          <input 
-            type="color"
-            id="color"
-            placeholder="Color"
-            autoComplete="off" />
-          <button type="button">Save</button>
-          <button type="button">Cancel</button>
-        </form>
-        <span className='Toggle-form'>New Word</span>
+        {
+          (this.state.showForm) ? 
+            <form>
+              <input 
+                type="text"
+                id="word"
+                placeholder="Word"
+                autoComplete="off" />
+              <input 
+                type="color"
+                id="color"
+                placeholder="Color"
+                autoComplete="off" />
+              <button type="button">Save</button>
+              <button type="button">Cancel</button>
+            </form>
+          :
+            <span className='Toggle-form'>New Word</span>
+        }
       </div>
     );
   }
