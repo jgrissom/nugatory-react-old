acef489d5b94c914169232cea92bca31a9cf0a7c
jgrissom <jgrissom91@gmail.com>
2024-02-08 19:21:19 -0600

    [5.28] Counter component - remove render() method - remove "this" keyword

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
-const Counter = props => {
-    render() { 
-        return ( <div className='counter'>words: { this.props.totalWords }</div> );
-    }
-}
+const Counter = props => <div className='counter'>words: { props.totalWords }</div>;
 
 export default Counter;
