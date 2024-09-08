e461dc4e2c1fdfb0789179d410ac9b405488561b
jgrissom <jgrissom91@gmail.com>
2024-09-08 12:06:31 -0500

    [5.25] Word component - remove render method - move props object destructuring - remove 'this' keyword

--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -2,6 +2,7 @@ import React, { useLayoutEffect, useRef } from 'react';
 
 function Word(props) {
   const wordEl = useRef();
+  const { onDelete, word } = props;
   
   // helper method
   const renderColor = (color) => (color === undefined || color === null ? 'black' : color);
@@ -16,21 +17,16 @@ function Word(props) {
     const el = wordEl.current;
     el.style.top = getRandomInt(0, window.innerHeight - el.clientHeight) + 'px';
     el.style.left = getRandomInt(0, window.innerWidth - el.clientWidth) + 'px';
-    console.log(`hello, ${props.word.word}`);
     // this is equivalent to componentWillUnmount
     return () => {
-      console.log(`goodbye, ${props.word.word}`);
     }
-  }, [wordEl, props.word.word]);
-
-  render() { 
-    const { onDelete, word } = this.props;
-    return (
-      <div ref={ this.wordEl } id={ word.id } onClick={ () => onDelete(word.id) } className='Word' style={{ color:this.renderColor(word.color) }}>
-        { word.word }
-      </div>
-    );
-  }
+  }, [wordEl, word.word])
+    
+  return (
+    <div ref={ wordEl } id={ word.id } onClick={ () => onDelete(word.id) } className='Word' style={{ color:renderColor(word.color) }}>
+      { word.word }
+    </div>
+  );
 }
 
 export default Word
